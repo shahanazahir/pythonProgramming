2aa0c61e87cdcf776633f72f2fe5bbe25481bf5a
shahanazahir <shahanasarangathil45@gmail.com>
2025-01-03 15:50:28 +0530

    Delete program7.py

--- program7.py
+++
@@ -1,4 +0,0 @@
-def length(words):
-  return max(len(word)for word in words)
-word_list=input("enter a list at words separated by spaces:").split()
-print("length of the largest word:",length(word_list))
